62072ef66cc3c4cc50a7197d431bca41371663a9
Aleksei Balandin <aleksisha@gmail.com>
2017-04-17 21:48:13 +0300

    add some functional

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,180 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <b-card>
+      <b-form-fieldset>
+        <b-form-radio v-model="filter.okato"
+                      :options="refs.okato">
+        </b-form-radio>
+      </b-form-fieldset>
+      <b-form-fieldset label="Район"
+                       horizontal>
+        <b-form-select v-model="filter.okato2"
+                       :options="refs.okato2">
+        </b-form-select>
+      </b-form-fieldset>
+      <b-form-fieldset label="Тип улицы"
+                       horizontal>
+        <b-form-select v-model="filter.streetType"
+                       :options="refs.streetType">
+        </b-form-select>
+      </b-form-fieldset>
+      <b-form-fieldset label="Содержит числительные"
+                       horizontal>
+        <b-form-checkbox v-model="filter.withNumbers"
+                      type="number">
+        </b-form-checkbox>
+      </b-form-fieldset>
+
+      <b-form-fieldset label="Шаблон названия"
+                       horizontal>
+        <b-form-input v-model="filter.streetNameTemplate">
+        </b-form-input>
+      </b-form-fieldset>
+
+      <b-btn variant="primary"
+             @click="searchStreets">Применить фильтр</b-btn>
+
+      <b-btn @click="clearForm">Очистить форму</b-btn>
+    </b-card>
+
+    <div v-if="foundStreets.items.length > 0"
+         class="justify-content-center row my-1">
+      <b-pagination size="md"
+                    :total-rows="foundStreets.items.length"
+                    :per-page="foundStreets.perPage"
+                    v-model="foundStreets.currentPage" />
+    </div>
+    <b-table v-if="foundStreets.items.length > 0"
+             striped
+             hover
+             :items="foundStreets.items"
+             :fields="foundStreets.fields"
+             :current-page="foundStreets.currentPage"
+             :per-page="foundStreets.perPage">
+      <template slot="type"
+                scope="item">
+        {{item.value}}
+      </template>
+      <template slot="name"
+                scope="item">
+        {{item.value}}
+      </template>
+    </b-table>
+
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello';
+import okatoRef from './data/okato.json';
+import streetRef from './data/moscow-streets.json';
+
+function okatoItemConverter(item) {
+  return {
+    value: item.value,
+    text: `[${item.value}] ${item.text}`,
+  };
+}
+
+const anyItem = {
+  value: '',
+  text: '[все]',
+};
+
+const streetTypeRef = [anyItem].concat(Object.getOwnPropertyNames(streetRef.reduce((res, item) => {
+  res[item.type] = true;
+  return res;
+}, {})));
+
+const data = {
+  filter: {
+    okato: '',
+    okato2: '',
+    streetType: '',
+    withNumbers: false,
+  },
+  refs: {
+    okato: [anyItem].concat(okatoRef
+      .filter(item => item.value.length < 6)
+      .map(okatoItemConverter)),
+    okato2: [
+      anyItem,
+    ],
+    streetType: streetTypeRef,
+  },
+  foundStreets: {
+    currentPage: 1,
+    perPage: 10,
+    items: [],
+    fields: {
+      type: {
+        label: 'тип',
+      },
+      name: {
+        label: 'наименование',
+      },
+      okato: {
+        label: 'ОКАТО',
+      },
+    },
+  },
+};
 
 export default {
   name: 'app',
+  data() {
+    return data;
+  },
   components: {
-    Hello,
+  },
+  watch: {
+    'filter.okato': function onOkatoChanged(val) {
+      const re = new RegExp(`^${val}`);
+      this.filter.okato2 = '';
+      this.refs.okato2 = [
+        anyItem,
+      ].concat(okatoRef
+        .filter(item => item.value !== val && re.test(item.value))
+        .map(okatoItemConverter));
+    },
+  },
+  methods: {
+    searchStreets() {
+      const app = this;
+      const okato = app.filter.okato2 || app.filter.okato;
+      const re = new RegExp(`^${okato}`);
+
+      function filterFn(item) {
+        if (okato && !re.test(item.okato)) {
+          return false;
+        }
+        if (app.filter.streetType && app.filter.streetType !== item.type) {
+          return false;
+        }
+        if (app.filter.withNumbers && !/\d/.test(item.name)) {
+          return false;
+        }
+        if (app.filter.streetNameTemplate) {
+          const reName = new RegExp(app.filter.streetNameTemplate.replace('*', '.*'), 'i');
+          if (!reName.test(item.name)) {
+            return false;
+          }
+        }
+        return true;
+      }
+
+      app.foundStreets.items = streetRef.filter(filterFn);
+    },
+    clearForm() {
+      const app = this;
+      app.filter.okato2 = '';
+      app.filter.okato = '';
+      app.filter.streetType = '';
+      app.filter.streetNameTemplate = '';
+    },
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+// import 'font-awesome/css/font-awesome.css';
 import App from './App';
 
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
 
 /* eslint-disable no-new */
 new Vue({
